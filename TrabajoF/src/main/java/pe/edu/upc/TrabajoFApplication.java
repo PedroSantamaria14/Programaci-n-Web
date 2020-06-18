6ca29d724714d7fb18d6240eefb225c47ddb09af
sergio031297 <53460095+sergio031297@users.noreply.github.com>
2020-06-18 01:27:59 -0500

    config

---
+++ TrabajoF/src/main/java/pe/edu/upc/TrabajoFApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabajoFApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabajoFApplication.class, args);
+	}
+
+}
